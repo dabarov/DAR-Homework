1999bf6ae50d270267760742bb0002b4650667ec
Aldiyar Dabarov <simplyalde@users.noreply.github.com>
2020-08-05 23:09:46 +0600

    Homework Video control buttons

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import "./Button.scss";
+import playIcon from "../../assets/play.svg";
+import stopIcon from "../../assets/stop.svg";
+import rightIcon from "../../assets/right.svg";
+import leftIcon from "../../assets/left.svg";
 
 type Props = {
   className: string;
+<<<<<<< Updated upstream
   text: string;
+=======
+  text?: string;
+>>>>>>> Stashed changes
   clickHandler?: () => void;
   type: "button" | "submit" | "reset";
+  image?: number;
 };
 
 export const Button: React.FunctionComponent<Props> = ({
@@ -21,6 +30,23 @@ export const Button: React.FunctionComponent<Props> = ({
       onClick={clickHandler}
     >
       {text}
+    </button>
+  );
+};
+
+export const SVGButton: React.FunctionComponent<Props> = ({
+  image,
+  type,
+  className,
+  clickHandler,
+}) => {
+  return (
+    <button
+      type={type}
+      className={"Button " + className}
+      onClick={clickHandler}
+    >
+      {image === 1 ? <img src={playIcon} alt=""/> : image === 2 ? <img src={stopIcon} alt=""/> : image === 3 ? <img src={leftIcon} alt=""/> : <img src={rightIcon} alt=""/>}
     </button>
   );
 };
--- src/pages/room/Room.tsx
+++ src/pages/room/Room.tsx
@@ -3,6 +3,7 @@ import YouTube from "react-youtube";
 import { Chat } from "../chat/Chat";
 
 import "./Room.scss";
+<<<<<<< Updated upstream
 import { UserContext } from "../../App";
 
 export const Room: React.FunctionComponent = () => {
@@ -12,6 +13,83 @@ export const Room: React.FunctionComponent = () => {
       <UserContext.Consumer>
         {({ user }) => <Chat user={user} />}
       </UserContext.Consumer>
+=======
+import { RoomHeader } from "../../components/room-header/RoomHeader";
+import { UserContext } from "../../services/context";
+import { useParams } from "react-router-dom";
+import { Button, SVGButton } from "../../components/button/Button";
+
+enum PlayerStates {
+  PLAYING = "PLAYING",
+  PAUSED = "PAUSED",
+}
+
+export const Room: React.FunctionComponent = () => {
+  const { id } = useParams();
+  const [player, setPlayer] = useState<any>(null);
+  const [playerState, setPlayerState] = useState<PlayerStates>();
+
+  const onVideoInit = (event: { target: any }) => {
+    setPlayer(event.target);
+  };
+
+  const toggleVideo = () => {
+    if (playerState !== PlayerStates.PLAYING) {
+      player?.playVideo();
+      setPlayerState(PlayerStates.PLAYING);
+    }
+    if (playerState === PlayerStates.PLAYING) {
+      player?.pauseVideo();
+      setPlayerState(PlayerStates.PAUSED);
+    }
+  };
+
+  const rewindForward = () => {
+    player?.seekTo(player?.getCurrentTime() + 10);
+  };
+
+  const rewindBack = () => {
+    player?.seekTo(player?.getCurrentTime() - 10);
+  };
+
+  return (
+    <div className="Room">
+      <RoomHeader />
+      <div className="room-wrapper">
+        <div>
+          <YouTube
+            className="youtube-video"
+            onReady={onVideoInit}
+            videoId={id}
+            containerClassName="video"
+          />
+          <div className="video-controls">
+            <SVGButton
+              className="video-btn"
+              clickHandler={rewindBack}
+              type="button"
+              image={3}
+            />
+            <SVGButton
+              className="video-btn"
+              clickHandler={toggleVideo}
+              type="button"
+              image={playerState !== PlayerStates.PLAYING ? 1 : 2}
+            />
+            <SVGButton
+              className="video-btn"
+              clickHandler={rewindForward}
+              type="button"
+              image={4}
+            />
+          </div>
+        </div>
+
+        <UserContext.Consumer>
+          {(value) => <Chat user={value?.user} />}
+        </UserContext.Consumer>
+      </div>
+>>>>>>> Stashed changes
     </div>
   );
 };
